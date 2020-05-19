bd39c31001f266db7de8e871aa35a321aa8fb656
Vadim Chernyak <vadimvc@gmail.com>
2020-05-19 02:16:52 -0400

    expanded on ballthrow

--- ballthrow.py
+++ ballthrow.py
@@ -1,4 +1,12 @@
 import math
+import numpy as np
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+
+
+import matplotlib.patches as patches
+import mpl_toolkits.mplot3d.art3d as art3d
+
 g=9.81 #m/s^2
 
 
@@ -6,7 +14,7 @@ g=9.81 #m/s^2
 def CalcInitialVelocity(x, z, alpha):
     #x y and z position the ball is being thrown from
     #x is going down the field, y is lateral, and z is height
-    # numbers are negative going down the field, z is up and y completes right hand rule
+    # numbers are positive going down the field, z is up and y completes right hand rule
     #alpha is the angle relative to the ground from which you're throwing the ball
     #all values are in meters
     
@@ -23,6 +31,7 @@ def CalcInitialVelocity(x, z, alpha):
     term3 = 1/(x*math.tan(alpha)-z)
 
     Velocity_squared=term1*term2*term3
+   # print('the velocity squared is' + str(Velocity_squared))
     Velocity = math.sqrt(Velocity_squared)
 
     return Velocity
@@ -38,17 +47,48 @@ def CalcBeta(x,y,targetx,targety):
     return beta
     #output is in degrees
     
-def CalculatePath(x,y,z,initial_velocity,alpha,beta):
-    ###
-    initial_velocity=CalcInitialVelocity(x,z,alpha)
+def CalculatePath(x,y,z,alpha,targetx,targety,targetz):
+    
+    #Calculate the initial velocity given the starting points and the end desired location
+    Vo=CalcInitialVelocity(x,z,alpha)
+    
+#########TODO CALCULATE THE ACTUAL RIGHT AMOUNT OF TIME THIS TAKES AND THEN HAVE THAT BE THE UPPER BOUND 
+    t=np.linspace(0, 5, num=100)
 
+    #Equations of motion
+  
+    Z=Vo*np.sin(t) - g*t
+    Y=np.linspace(y,targety,num=100)
+    X=Vo*np.cos(t)
 
+    return X,Y,Z
+
+def PlotPath(x,y,z,alpha,targetx,targety,targetz):
+    (x,y,z)=CalculatePath(x,y,z,alpha,targetx,targety,targetz)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.plot3D(x, y, z, 'red')
+
+#######TODO MAKE THIS PATCH WORK CORRECTLY
+    # rect = patches.Rectangle((0,0),0.1,.18,linewidth=1,edgecolor='blue',facecolor='blue')
+    # ax.add_patch(rect)
+    # art3d.pathpatch_2d_to_3d(rect, z=0, zdir="z")
+
+    
+    plt.title('Ball Trajectory' )  
+    ax.set_xlabel('Ball as it\'s flying in x direction')  
+    ax.set_ylabel('Ball as it\'s flying in y direction')
+    ax.set_zlabel('Ball as it\'s flying in z direction')
+    plt.show()
 
 #test case
-x=-10; y=10; z=10; alpha=60
-targetx=0;targety=0
+x=10; y=12; z=2; alpha=60
+targetx=0;targety=0; targetz=6
+
+PlotPath(x,y,z,alpha,targetx,targety,targetz)
 
 
-
+#(x,y,z)=CalculatePath(x,y,z,alpha,targetx,targety,targetz)
+#print(x)
 #CalcBeta(x,y,targetx,targety)
 #print(CalcInitialVelocity(x,z,alpha))
